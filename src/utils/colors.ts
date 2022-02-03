6b8553f7515b842395fed6cb7e63461df14b9789
Karlo A. Lopez <karlo.lopez@syngenta.com>
2022-02-02 21:15:53 -0600

    feat: colors.ts

---
+++ src/utils/colors.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+    PrimaryColor: '#81717A',
+    SecondaryColor: '#9D8CA1',
+    ThirdColor: '#9993B2',
+    FourthColor: '#A7ABDD',
+    FifthColor: '#B4D4EE',
+}
+
+//Courtesy of https://coolors.co/ ;)

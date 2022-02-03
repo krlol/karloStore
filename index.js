a94d49804a28a59183f820058f1567203ab69e0d
Karlo A. Lopez <karlo.lopez@syngenta.com>
2022-02-02 23:20:58 -0600

    chore: some config, missing dependency for animation

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App.tsx';
 import {name as appName} from './app.json';
 import {Provider} from 'react-redux';
-import {store} from './src/redux/store';
+import store from './src/redux/store';
 
 const RnRedux = () => (
   <Provider store={store}>

a14f705bf9149b74042bb64bb451eb960a58cb06
Karlo A. Lopez <karlo.lopez@syngenta.com>
2022-02-03 17:04:57 -0600

    fix: reanimated on android

--- android/app/src/main/java/com/karlostore/MainApplication.java
+++ android/app/src/main/java/com/karlostore/MainApplication.java
@@ -1,14 +1,13 @@
 package com.karlostore;
 
 import android.app.Application;
-import android.content.Context;
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -33,6 +32,10 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+          @Override
+          protected JSIModulePackage getJSIModulePackage() {
+              return new ReanimatedJSIModulePackage(); // <- add
+          }
       };
 
   @Override

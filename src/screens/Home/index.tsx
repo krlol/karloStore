f50d7ce030afce91c5156a6583a6c7938775f28f
Karlo A. Lopez <karlo.lopez@syngenta.com>
2022-02-02 21:51:06 -0600

    feat: searchBar component implementation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import SearchBar from 'components/SearchBar';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { Strings } from 'utils/strings';
+import styles from './styles';
 
 export default function HomeScreen() {
-  return (
-    <View><Text>{`${Strings.Home} ${Strings.Screen}`}</Text></View>
-  )
+
+  const [searchValue, setSearchValue] = useState('');
+
+  return (<ScrollView style={styles.scrollView}>
+    <View style={styles.headerContainer}>
+      <SearchBar
+        placeholder={Strings.SearchPlaceHolder}
+        value={searchValue}
+        onChangeText={(text)=>setSearchValue(text)}
+        onSubmit={()=>{}}
+      />
+    </View>
+    <View style={styles.chipsContainer}>
+
+    </View>
+    
+  </ScrollView>)
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,16 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    scrollView:{
+        paddingTop: 25,
+        marginHorizontal: 15,
+    },
+    headerContainer:{
+
+    },
+    chipsContainer:{
+        
+    }
 })
 
 export default styles;
